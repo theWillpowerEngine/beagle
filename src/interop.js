5db55c3a4d7e32354d1cb5559f9d9668c46fff44
Dan Larsen <dlarsen023@yahoo.com>
2022-01-12 20:35:06 -0800

    File location fix for the electron packaging (so the .exe version works)

--- src/interop.js
+++ src/interop.js
@@ -4,6 +4,19 @@ const fs = require('fs')
 module.exports = (win) => {
     let browser = win
     let fullscreen = true
+    let rootPath = "."
+
+    fs.readdir("./zelazny/nodes", (err, f) => {
+        if(err) {
+            fs.readdir("./resources/app/zelazny/nodes", (err2, f2) => {
+                if(!err2)
+                    rootPath = "./resources/app"
+                else
+                    throw "Support files not found"
+            })
+        }
+    })
+
 
     //////////////////////////////
     //Browser Interop
@@ -20,7 +33,7 @@ module.exports = (win) => {
     //File System Stuff
     ipcMain.handle("zelazny", (event, topic) => {
         try {
-            var data = fs.readFileSync(`zelazny\\nodes\\${topic}.n`, 'utf8')
+            var data = fs.readFileSync(rootPath + `\\zelazny\\nodes\\${topic}.n`, 'utf8')
             return data
         } catch(ex) {
             return "Zelazny '" + topic + "' was not found"
